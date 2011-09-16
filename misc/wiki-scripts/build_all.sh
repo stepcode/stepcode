553091cfaeb7749bc314939e4e1174490e36159a
Mark Pictor <mpictor@gmail.com>
2011-09-15 21:00:09 -0400

    wiki links finally work... I think

--- misc/wiki-scripts/build_all.sh
+++ misc/wiki-scripts/build_all.sh
@@ -12,7 +12,7 @@ mk="make -j4"
 schemas="../data/example/example.exp;../data/ap227/ap227.exp;../data/ap214e3/AP214E3_2010.exp;../data/select.exp;../data/ap203e2/ap203e2_mim_lf.exp;../data/ap203/203wseds.exp;../data/ifc2x3/IFC2X3_TC1.exp;../data/pdmnet.exp;../data/ifc2x4/IFC2X4_RC2.exp;/opt/step/notingit/part409cdts_wg3n2617mim_lf.exp;/opt/step/notingit/ap242_managed_model_based_3d_engineering_mim_lf.exp;/opt/step/notingit/AP224E3_wg3n1941.exp;/opt/step/notingit/AP238E2_aim_lf_20100903.exp"
 
 #count warnings and errors, append to $matrix_file. creates hypertext links to stderr,stdout txt
-# $1 is the name of the row, $2 is the path and first part of the filename
+# $1 is the name of the row, $2 is the path and first part of the filename, $3 is the schema
 function count_we {
     e="$2"_stderr.txt
     o="$2"_stdout.txt
@@ -22,9 +22,7 @@ function count_we {
             echo "<td><font color="red">`grep -ci error $e` errors</font></td>"
             echo "<td><font color="blue">`grep -ci warning $e` warnings</font></td>"
             if [ -s $e ]; then
-                echo -n "<td><a href="
-                echo $e | sed -e "s|$result_dir/||;" -e "s/^\(.*\)\.txt$/\1/;"
-                echo ">stderr text</a></td>"
+                echo -n "<td><a href=schema_$3#wiki-$1_e>stderr text</a></td>"
             else
                 echo "<td>   ----   </td>"
             fi
@@ -36,9 +34,7 @@ function count_we {
             echo "<td>   ----   </td>"
         fi
         if [ -s $o ]; then
-            echo -n "<td><a href="
-            echo $o | sed -e "s|$result_dir/||;"
-            echo ">stdout text</a></td>"
+            echo -n "<td><a href=schema_$3wiki-#$1_o>stdout text</a></td>"
         else #empty
             echo "<td>   ----   </td>"
         fi
@@ -48,36 +44,36 @@ function count_we {
 
 #make fedex warnings and errors pretty
 function fedex_details {
-    (
-        i="$result_dir/fedex_$1_stderr.txt"
-        w=`sed -ne 's|^.*:\([0-9]\+\): WARNING: \(.*\)$|<tr><td>\1</td><td>\2</td></tr>|p;' $i`
-        e=`sed -ne 's|^.*:\([0-9]\+\): --ERROR: \(.*\)$|<tr><td>\1</td><td>\2</td></tr>|p;' $i`
-        if [ "x$w" != "x" ]; then
-            echo "## fedex_plus warnings"
-            echo "<table  width=100%><tr><th>Line</th><th>Message</th>$w</table>"
-            echo 
-        fi
-        if [ "x$e" != "x" ]; then
-            echo "## fedex_plus errors"
-            echo "<table  width=100%><tr><th>Line</th><th>Message</th>$e</tr></table>"
-            echo
-        fi
-        o=`grep -ve ": WARNING:" -e ": --ERROR:" -e ^$ $i`
-        if [ "x$o" != "x" ]; then
-            echo "## Other text from Standard Error"
-            echo "<pre>"
-            sed -e 's|^.*:[0-9]\+: WARNING: .*$||g;' -e 's|^.*:[0-9]\+: --ERROR: .*$||g;' -e 's|$|\n|;' -e 's/\t/    /;' $i|grep -v ^$
-            echo "</pre>"
-            echo
-        fi
-        if [ -s $result_dir/fedex_$1_stdout.txt ]
-            echo "## Standard Output"
-            cat $result_dir/fedex_$1_stdout.txt
-            echo
-        fi
-
-        # TODO rename file, put everything for one schema in one file
-    ) >$out_dir/fedex_$1_stderr.md
+    i="$result_dir/fedex_$1_stderr.txt"
+    w=`sed -ne 's|^.*:\([0-9]\+\): WARNING: \(.*\)$|<tr><td>\1</td><td>\2</td></tr>|p;' $i`
+    e=`sed -ne 's|^.*:\([0-9]\+\): --ERROR: \(.*\)$|<tr><td>\1</td><td>\2</td></tr>|p;' $i`
+    if [ "x$w" != "x" ]; then
+        echo "<a name=\"fedex_plus_w\">" #enable jumping to this location in the file
+        echo "### fedex_plus warnings"
+        echo "</a><table  width=100%><tr><th>Line</th><th>Message</th>$w</table>"
+        echo
+    fi
+    if [ "x$e" != "x" ]; then
+        echo "<a name=\"fedex_plus_e\">"
+        echo "### fedex_plus errors"
+        echo "</a><table  width=100%><tr><th>Line</th><th>Message</th>$e</tr></table>"
+        echo
+    fi
+    o=`grep -ve ": WARNING:" -e ": --ERROR:" -e ^$ $i`
+    if [ "x$o" != "x" ]; then
+        echo "### Other text from Standard Error"
+        echo "<pre>"
+        sed -e 's|^.*:[0-9]\+: WARNING: .*$||g;' -e 's|^.*:[0-9]\+: --ERROR: .*$||g;' -e 's|$|\n|;' -e 's/\t/    /;' $i|grep -v ^$
+        echo "</pre>"
+        echo
+    fi
+    if [ -s $result_dir/fedex_$1_stdout.txt ]; then
+        echo "<a name=\"fedex_plus_o\">"
+        echo "### Standard Output"
+        echo "</a><pre>"
+        cat $result_dir/fedex_$1_stdout.txt
+        echo "</pre>"
+    fi
 }
 
 function build_one_schema {
@@ -108,40 +104,69 @@ function build {
     done
 }
 
+function make_schema_page {
+    # $1 is schema name, i.e. AUTOMOTIVE_DESIGN, $2 is schema *file* name, i.e. ap214e3_mim_lf.exp
+    echo "## File $2"
+    echo "### Schema $1"
+    fedex_details $1
+    echo "<a name=\"lib_e\"></a><a name=\"lib_w\"></a>"
+    echo "### stderr from compiling lib"
+    if [ -s $result_dir/compile_libsdai_${1}_stderr.txt ]; then
+        echo "<pre>"
+        cat $result_dir/compile_libsdai_${1}_stderr.txt
+        echo "</pre>"
+    fi
+    echo "<a name=\"p21read_e\"></a><a name=\"p21read_w\"></a>"
+    echo "### stderr from compiling p21read"
+    if [ -s $result_dir/compile_p21read_sdai_${1}_stderr.txt ]; then
+        echo "<pre>"
+        cat $result_dir/compile_p21read_sdai_${1}_stderr.txt
+        echo "</pre>"
+    fi
+    echo "### end"
+}
+
 function gen_wiki {
     echo "## `date`" >$matrix_file
     git log --decorate=short -n1 |\
         head -n1 |\
         sed -e 's|^.*commit \([a-z0-9]\+\) .*$|### Current as of commit [\1](http://github.com/mpictor/StepClassLibrary/commit/\1)|;' >>$matrix_file
     echo "<table border=1>" >>$matrix_file
+    echo "<tr><th>Key</th></tr><tr><td><table width=100%>" >>$matrix_file
+    echo "<tr><th>Item</th><th>Description</th></tr>" >>$matrix_file
+    echo "<tr><td>fedex_plus</td><td>Generate c++ from an EXPRESS schema</td></tr>" >>$matrix_file
+    echo "<tr><td>lib</td><td>Compile the generated c++ into a library</td></tr>" >>$matrix_file
+    echo "<tr><td>p21read</td><td>Compile a p21read app, linked against the above library</td></tr>" >>$matrix_file
+    echo "</table></td></tr>" >>$matrix_file
     for h in `echo $schemas | sed -e 's/;/\n/g;'`
     do
         i=`sed -ne '0,/^\s*SCHEMA/s/^.*SCHEMA\s\+\(.*\);.*$/\1/p;' $h | tr a-z A-Z`
 
         #create a table for this schema
         echo "<tr><td><table width=100%><tr><th>Schema $i</th></tr><tr><td>" >>$matrix_file
-        j=`echo $1|sed -e "s|^.*/\(.*\.exp\)$|\1|;"`  # strip path from .exp file name
+        j=${h##*/} #schema file name, without dirs
         echo "$j</td></tr><tr><td><table border=1>" >>$matrix_file
-        count_we "fedex_plus (generate c++)" $result_dir/fedex_$i
-        count_we "gcc (compile lib)" $result_dir/compile_libsdai_$i
-        count_we "gcc (compile p21read)" $result_dir/compile_p21read_sdai_$i
+        count_we "fedex_plus" $result_dir/fedex_$i $i
+        count_we "lib" $result_dir/compile_libsdai_$i $i
+        count_we "p21read" $result_dir/compile_p21read_sdai_$i $i
         echo "</table></td></tr>" >>$matrix_file
         echo "</table></td></tr>" >>$matrix_file
-        if [ -s $result_dir/fedex_"$i"_stderr.txt ]; then
-            fedex_details $i
-        fi
+#         if [ -s $result_dir/fedex_"$i"_stderr.txt ]; then
+#             fedex_details $i
+#         fi
+        make_schema_page $i $j >$out_dir/schema_$i.md
     done
     echo "</table>" >>$matrix_file
-    echo "Done!"
-    echo "Copy everything in build_all/$out_dir to the wiki"
 }
 
-
+# if pwd is build_all, assume everything is up to date and just create wiki pages
 if [ ! ${PWD##*/} == "build_all" ]; then
     mkdir -p build_all && cd build_all && rm -rf * && mkdir -p $out_dir && mkdir -p $result_dir
     build
     echo -n "Done building. "
 fi
 
-echo "Now creating wiki pages"
+echo "Creating wiki pages..."
 gen_wiki
+echo "Done!"
+echo "Copy everything in build_all/$out_dir to the wiki"
