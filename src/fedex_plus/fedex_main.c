6851b271e5bd3521a4bd842292150a042fb343fc
Mark Pictor <mpictor@gmail.com>
2011-09-18 16:49:31 -0400

    include git commit description in fedex_plus version output

--- src/fedex_plus/fedex_main.c
+++ src/fedex_plus/fedex_main.c
@@ -77,14 +77,15 @@
 #include "../express/express.h"
 #include "../express/resolve.h"
 
-static void
-fedex_plus_usage() {
+extern void print_fedex_version( void );
+
+static void fedex_plus_usage() {
     fprintf( stderr, "usage: %s [-s|-S] [-a|-A] [-c|-C] [-L] [-v] [-d # | -d 9 -l nnn -u nnn] [-n] [-p <object_type>] {-w|-i <warning>} express_file\n", EXPRESSprogram_name );
     fprintf( stderr, "where\t-s or -S uses only single inheritance in the generated C++ classes\n" );
     fprintf( stderr, "\t-a or -A generates the early bound access functions for entity classes the old way (without an underscore)\n" );
     fprintf( stderr, "\t-c or -C generates C++ classes for use with CORBA (Orbix)\n" );
     fprintf( stderr, "\t-L prints logging code in the generated C++ classes\n" );
-    fprintf( stderr, "\t-v produces a version description\n" );
+    fprintf( stderr, "\t-v produces the version description below\n" );
     fprintf( stderr, "\t-d turns on debugging (\"-d 0\" describes this further\n" );
     fprintf( stderr, "\t-p turns on printing when processing certain objects (see below)\n" );
     fprintf( stderr, "\t-n do not pause for internal errors (useful with delta script)\n" );
@@ -104,6 +105,7 @@ fedex_plus_usage() {
     fprintf( stderr, "	s	schema or file\n" );
     fprintf( stderr, "	#	pass #\n" );
     fprintf( stderr, "	E	everything (all of the above)\n" );
+    print_fedex_version();
     exit( 2 );
 }
 
@@ -121,8 +123,7 @@ int success( Express model ) {
 
 /* This function is called from main() which is part of the NIST Express
    Toolkit.  It assigns 2 pointers to functions which are called in main() */
-void
-EXPRESSinit_init() {
+void EXPRESSinit_init() {
     EXPRESSbackend = print_file;
     EXPRESSsucceed = success;
     EXPRESSgetopt = Handle_FedPlus_Args;
