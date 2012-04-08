c2b87f8f0ba054e53e89a792eb11926cdb00774c
Mark Pictor <mpictor@gmail.com>
2012-04-08 12:09:24 -0400

    include scl_trace_fprintf.h in source in src/fedex_plus
    only added to files which use fprintf

--- src/fedex_plus/classes.c
+++ src/fedex_plus/classes.c
@@ -29,6 +29,8 @@ N350 ( August 31, 1993 ) of ISO 10303 TC184/SC4/WG7.
 #include <stdlib.h>
 #include "classes.h"
 
+#include <scl_trace_fprintf.h>
+
 int isAggregateType( const Type t );
 int isAggregate( Variable a );
 Variable VARis_type_shifter( Variable a );
