48f1285e03d690060bb41141a11de48c37ed1287
Mark Pictor <mpictor@gmail.com>
2013-07-30 18:20:06 -0400

    improve stepattr copy constructor
    uses shallow copy, so access violations are possible

--- src/clstepcore/STEPattribute.inline.cc
+++ src/clstepcore/STEPattribute.inline.cc
@@ -12,13 +12,63 @@
 
 #include <assert.h>
 #include <STEPattribute.h>
+#include <STEPaggregate.h>
 #include <sdai.h>
 #include <ExpDict.h>
 #include "sc_memmgr.h"
 
-///  This is needed so that STEPattribute's can be passed as references to inline functions
+/// This is needed so that STEPattribute's can be passed as references to inline functions
+/// NOTE this code only does shallow copies. It may be necessary to do more, in which case
+/// the destructor and assignment operator will also need examined.
 STEPattribute::STEPattribute( const STEPattribute & a )
-    : _derive( false ), _redefAttr( 0 ), aDesc( a.aDesc ), refCount( 0 ) {}
+    : _derive( a._derive ), _redefAttr( a._redefAttr ), aDesc( a.aDesc ), refCount( a.refCount ) {
+
+    //the following comes from STEPattribute::ShallowCopy
+    switch( NonRefType() ) {
+        case INTEGER_TYPE:
+            *ptr.i = *( a.ptr.i );
+            break;
+
+        case BINARY_TYPE:
+            *( ptr.b ) = *( a.ptr.b );
+            break;
+
+        case STRING_TYPE:
+            *( ptr.S ) = *( a.ptr.S );
+            break;
+
+        case REAL_TYPE:
+        case NUMBER_TYPE:
+            *ptr.r = *( a.ptr.r );
+            break;
+
+        case ENTITY_TYPE:
+            ptr.c = a.ptr.c; //NOTE may need to deep copy
+            break;
+
+        case AGGREGATE_TYPE:
+        case ARRAY_TYPE:      // DAS
+        case BAG_TYPE:        // DAS
+        case SET_TYPE:        // DAS
+        case LIST_TYPE:       // DAS
+            ptr.a = a.ptr.a; //NOTE may need to deep copy
+            break;
+
+        case SELECT_TYPE:
+            *ptr.sh = *( a.ptr.sh );
+            break;
+
+        case ENUM_TYPE:
+        case BOOLEAN_TYPE:
+        case LOGICAL_TYPE:
+            ptr.e->put( a.ptr.e->asInt() );
+            break;
+
+        default:
+            *ptr.u = *( a.ptr.u );
+            break;
+    }
+}
 
 ///  INTEGER
 STEPattribute::STEPattribute( const class AttrDescriptor & d, SDAI_Integer * p )
