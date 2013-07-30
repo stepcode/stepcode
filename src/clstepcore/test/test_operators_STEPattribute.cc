bcd01474cd22fc87b6b4e88ee6c9f5c1c6374fd9
Mark Pictor <mpictor@gmail.com>
2013-07-30 18:19:41 -0400

    skeleton for the operators/constructors tests for STEPattribute

---
+++ src/clstepcore/test/test_operators_STEPattribute.cc
@@ -0,0 +1,9 @@
+///test constructors, destructor, shallow copy, assignment operators for STEPattribute
+
+#include <STEPattribute.h>
+
+int main( int /*argc*/, char ** /*argv*/ ) {
+    STEPattribute a, b, c;
+
+    exit( EXIT_FAILURE );
+}
