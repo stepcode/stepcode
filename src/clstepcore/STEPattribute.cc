ae55ca80a3d8f5b635dc461be607ca218e2e9bac
Mark Pictor <mpictor@gmail.com>
2013-10-29 20:44:05 -0400

    oops, STEPattribute needs setters now that ptr is private

--- src/clstepcore/STEPattribute.cc
+++ src/clstepcore/STEPattribute.cc
@@ -829,6 +829,8 @@ bool STEPattribute::is_null()  const {
     }
 }
 
+// these get the attr value
+
 SDAI_Integer * STEPattribute::Integer(){
     if( NonRefType() == INTEGER_TYPE ) {
         return ptr.i;
@@ -913,6 +915,84 @@ SCLundefined * STEPattribute::Undefined() {
     }
     return 0;
 }
+
+// these set the attr value
+
+void STEPattribute::Integer( SDAI_Integer * n ) {
+    assert( NonRefType() == INTEGER_TYPE );
+    assert( ptr.i == 0 );
+    ptr.i = n;
+}
+
+void STEPattribute::Real( SDAI_Real * n ) {
+    assert( NonRefType() == REAL_TYPE );
+    assert( ptr.r == 0 );
+    ptr.r = n;
+}
+
+void STEPattribute::Number( SDAI_Real * n ) {
+    assert( NonRefType() == NUMBER_TYPE );
+    assert( ptr.r == 0 );
+    ptr.r = n;
+}
+
+void STEPattribute::String( SDAI_String * str ) {
+    assert( NonRefType() == STRING_TYPE );
+    assert( ptr.S == 0 );
+    ptr.S = str;
+}
+
+void STEPattribute::Binary( SDAI_Binary * bin ) {
+    assert( NonRefType() == BINARY_TYPE );
+    assert( ptr.b == 0 );
+    ptr.b = bin;
+}
+
+void STEPattribute::Entity( SDAI_Application_instance * ent ) {
+    assert( NonRefType() == ENTITY_TYPE );
+    assert( ptr.c == 0 );
+    ptr.c = new (SDAI_Application_instance * );
+    *( ptr.c ) = ent;
+}
+
+void STEPattribute::Aggregate( STEPaggregate * aggr ) {
+    assert( ( NonRefType() == AGGREGATE_TYPE ) || ( NonRefType() == ARRAY_TYPE ) || ( NonRefType() == BAG_TYPE )
+    || ( NonRefType() == SET_TYPE ) || ( NonRefType() == LIST_TYPE ) );
+    assert( ptr.a == 0 );
+    ptr.a = aggr;
+}
+
+void STEPattribute::Enum( SDAI_Enum * enu ) {
+    assert( NonRefType() == ENUM_TYPE );
+    assert( ptr.e == 0 );
+    ptr.e = enu;
+}
+
+void STEPattribute::Logical( SDAI_LOGICAL * log ) {
+    assert( NonRefType() == LOGICAL_TYPE );
+    assert( ptr.e == 0 );
+    ptr.e = log;
+}
+
+void STEPattribute::Boolean( SDAI_BOOLEAN * boo ) {
+    assert( NonRefType() == BOOLEAN_TYPE );
+    assert( ptr.e == 0 );
+    ptr.e = boo;
+}
+
+void STEPattribute::Select( SDAI_Select * sel ) {
+    assert( NonRefType() == SELECT_TYPE );
+    assert( ptr.sh == 0 );
+    ptr.sh = sel;
+}
+
+void STEPattribute::Undefined( SCLundefined * undef ) {
+    //FIXME is this right, or is the Undefined() above right?
+    assert( NonRefType() == REFERENCE_TYPE || NonRefType() == UNKNOWN_TYPE );
+    assert( ptr.u == 0 );
+    ptr.u = undef;
+}
+
 
 /** evaluate the equality of two attributes
  * ignores _error and refCount, since those are ancillary
