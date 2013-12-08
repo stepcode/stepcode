ca560239cefc4ca7b21bb93a95f2386a2ef4c0fd
Mark Pictor <mpictor@gmail.com>
2013-12-08 11:12:31 -0500

    use lazyRefs in lazyInstMgr

--- src/cllazyfile/lazyInstMgr.h
+++ src/cllazyfile/lazyInstMgr.h
@@ -159,13 +159,17 @@ class lazyInstMgr {
 
         //list all instances that one instance depends on (recursive)
         instanceSet * instanceDependencies( instanceID id );
+        bool isLoaded( instanceID id ) {
+            _instancesLoaded.find( id );
+            return _instancesLoaded.success();
+        }
 
         const char * typeFromFile( instanceID id ) {
             instanceStreamPos_t::cvector * cv;
             cv = _instanceStreamPos.find( id );
             if( cv ) {
                 if( cv->size() != 1 ) {
-                    std::cerr << "Error at " << __FILE__ << ":" << __LINE__ << " - multiple instances with one instanceID not supported yet." << std::endl;
+                    std::cerr << "Error at " << __FILE__ << ":" << __LINE__ << " - multiple instances (" << cv->size() << ") with one instanceID (" << id << ") not supported yet." << std::endl;
                     return 0;
                 }
                 positionAndSection ps = cv->at( 0 );
@@ -174,6 +178,8 @@ class lazyInstMgr {
                 sectionID sid = ps >> 48;
                 return _dataSections[sid]->getType( off );
             }
+            std::cerr << "Error at " << __FILE__ << ":" << __LINE__ << " - instanceID " << id << " not found." << std::endl;
+            return 0;
         }
 
         // TODO implement these
