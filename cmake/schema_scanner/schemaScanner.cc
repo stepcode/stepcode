86302fb0f3886f6c54e8fddd18cc096f21eee85a
Mark Pictor <mpictor@gmail.com>
2014-01-21 19:41:13 -0500

    take a stab at writing a CMakeLists.txt for the schema

--- cmake/schema_scanner/schemaScanner.cc
+++ cmake/schema_scanner/schemaScanner.cc
@@ -7,17 +7,26 @@
  * generated files will be called.
  */
 
+extern "C" {
 #include "expparse.h"
 #include "expscan.h"
-
 #include "express/scope.h"
 #include "genCxxFilenames.h"
+#include "sc_mkdir.h"
+
 #include <string.h>
+}
+
 #include <string>
 #include <sstream>
 #include <iomanip>
+#include <fstream>
 
 int multiple_inheritance = 0;
+
+using std::string;
+using std::stringstream;
+using std::endl;
 
 bool isBuiltin( const Type t ) {
     switch( TYPEget_body( t )->type ) {
@@ -48,27 +57,77 @@ bool isBuiltin( const Type t ) {
     return false;
 }
 
+void writeLists( const char * schema_name, stringstream & eh, stringstream & ei, stringstream & th, stringstream & ti ) {
+    sc_mkdir( schema_name );
+    string cmListsPath = schema_name;
+    cmListsPath += "/CMakeLists.txt";
+    std::ofstream cmLists;
+    cmLists.open( cmListsPath.c_str() );
+
+    cmLists << "# -----  GENERATED FILE  -----" << endl;
+    cmLists << "# -----   Do not edit!   -----" << endl << endl;
+    cmLists << "PROJECT( sdai_" << schema_name << " )" << endl << endl;
+    cmLists << "# headers to be installed - 3 lists - entity, type, misc" << endl << endl;
+
+    cmLists << "set( " << schema_name << "_entity_hdrs" << endl;
+    cmLists << eh;
+    cmLists << "   )" << endl << endl;
+
+    cmLists << "set( " << schema_name << "_type_hdrs" << endl;
+    cmLists << th;
+    cmLists << "   )" << endl << endl;
+
+    cmLists << "set( " << schema_name << "_misc_hdrs" << endl;
+    cmLists << " TODO name the headers here "; //TODO
+    cmLists << "   )" << endl << endl;
+
+    cmLists << "# install all headers" << endl;
+    cmLists << "install( FILES \"${" << schema_name << "_entity_hdrs} ${" << schema_name;
+    cmLists << "_type_hdrs} ${" << schema_name << "_misc_hdrs}\"" << endl;
+    cmLists << "         DESTINATION \"schemas/" << schema_name << "\" )" << endl << endl;
+
+    cmLists << "# implementation files - 3 lists" << endl << endl;
+
+    cmLists << "set( " << schema_name << "_entity_impls" << endl;
+    cmLists << ei;
+    cmLists << "   )" << endl << endl;
+
+    cmLists << "set( " << schema_name << "_type_impls" << endl;
+    cmLists << ti;
+    cmLists << "   )" << endl << endl;
+
+    cmLists << "set( " << schema_name << "_misc_impls" << endl;
+    cmLists << " TODO name the files here "; //TODO
+    cmLists << "   )" << endl << endl;
+
+    cmLists << "# targets, logic, etc are within a set of macros shared by all schemas" << endl;
+    cmLists << "include( \"cmake/schemaMacros.cmake\" )" << endl;
+    cmLists << "BUILD_SCHEMA( \"" << schema_name << "\" \"${" << schema_name << "_entity_impls} ";
+    cmLists << "${" << schema_name << "_type_impls} ${" << schema_name << "_misc_impls}\" )" << endl;
+
+
+    //TODO assemble absolute path to schema dir and print it to stdout, so CMake can use it with add_subdirectory()
+}
+
 void printSchemaFilenames( Schema sch ){
-    std::stringstream typeHeaders, typeImpls, entityHeaders, entityImpls;
+    stringstream typeHeaders, typeImpls, entityHeaders, entityImpls;
     int ecount = 0, tcount = 0;
 
     DictionaryEntry de;
     Generic x;
     filenames_t fn;
-    /* surround the schema name with colons to make it easier to parse the output */
-    //     printf( ":%s:;", sch->symbol.name );
     DICTdo_init( sch->symbol_table, &de );
     while( 0 != ( x = DICTdo( &de ) ) ) {
         switch( DICT_type ) {
             case OBJ_ENTITY:
                 fn = getEntityFilenames( ( Entity ) x );
-//                 printf( "%s;%s;", fn.impl, fn.header );
                 entityHeaders << std::setw( 30 ) << fn.header;
                 entityImpls << std::setw( 30 ) << fn.impl;
                 ++ecount;
                 if( ( ecount % 4 ) == 0 ) {
-                    entityHeaders << std::endl;
-                    entityImpls << std::endl;
+                    // 4 columns
+                    entityHeaders << endl;
+                    entityImpls << endl;
                 }
                 break;
             case OBJ_TYPE: {
@@ -83,16 +142,12 @@ void printSchemaFilenames( Schema sch ){
                     break;
                 }
                 fn = getTypeFilenames( t );
-                if( strcmp( fn.impl, "type/EntityAggregate.cc") == 0 ) {
-                    asm("nop");
-                }
-                //                 printf( "%s;%s;", fn.impl, fn.header );
                 typeHeaders << std::setw( 30 ) << fn.header;
                 typeImpls << std::setw( 30 ) << fn.impl;
                 ++tcount;
                 if( ( tcount % 4 ) == 0 ) {
-                    typeHeaders << std::endl;
-                    typeImpls << std::endl;
+                    typeHeaders << endl;
+                    typeImpls << endl;
                 }
                 break;
             }
@@ -105,13 +160,7 @@ void printSchemaFilenames( Schema sch ){
                 break;
         }
     }
-//     printf( "\n" );
-    //now, write the CMakeLists.txt
-    std::string schemaName = sch->symbol.name;
-    std::ostream cml( schemaName + "/CMakeLists.txt" );
-    // TODO create header here
-    //cml << "
-    //one cmakelists, or 3+?
+    //write the CMakeLists.txt
     writeLists( sch->symbol.name, entityHeaders, entityImpls, typeHeaders, typeImpls );
 }
 
@@ -123,9 +172,9 @@ int main( int argc, char ** argv ) {
     /* copied from fedex.c */
     Express model;
     if( ( argc != 2 ) || ( strlen( argv[1] ) < 1 ) ) {
-        fprintf( stderr, "\nUsage: %s file.exp\nOutput: one line per schema as follows,", argv[0] );
-        fprintf( stderr, " containing file names for entities and types\n" );
-        fprintf( stderr, ":schema_name:;entity/e_name.h;entity/e_name.cc;type/t_name.h;type/t_name.cc;...;\n" );
+        fprintf( stderr, "\nUsage: %s file.exp\nOutput: a CMakeLists.txt to build the schema,", argv[0] );
+        fprintf( stderr, " containing file names for entities, types, etc\n" );
+        fprintf( stderr, "also prints (to stdout) the absolute path to the directory CMakeLists.txt was created in\n" );
         exit( EXIT_FAILURE );
     }
     EXPRESSprogram_name = argv[0];
