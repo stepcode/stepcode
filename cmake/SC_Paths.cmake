5384fe0dbba6be85791e1bbe97ca05027715ac2a
Mark Pictor <mpictor@gmail.com>
2014-01-27 09:27:23 -0500

    fix locate_schema macro

--- cmake/SC_Paths.cmake
+++ cmake/SC_Paths.cmake
@@ -14,20 +14,22 @@ MACRO(RELATIVE_PATH_TO_TOPLEVEL current_dir rel_path)
   endif(PATH_LENGTH GREATER 0)
 ENDMACRO(RELATIVE_PATH_TO_TOPLEVEL current_dir rel_path)
 
-MACRO( LOCATE_SCHEMA SCHEMA_FILE )
+MACRO( LOCATE_SCHEMA SCHEMA_FILE _res_var )
   if( EXISTS "${CMAKE_BINARY_DIR}/${SCHEMA_FILE}" )  #is it a path relative to build dir?
-    set( SCHEMA_FILE "${CMAKE_BINARY_DIR}/${SCHEMA_FILE}" )
+    set( ${_res_var} "${CMAKE_BINARY_DIR}/${SCHEMA_FILE}" )
   elseif( EXISTS "${SC_SOURCE_DIR}/data/${SCHEMA_FILE}" )  # path relative to STEPcode/data?
-    set( SCHEMA_FILE "${SC_SOURCE_DIR}/data/${SCHEMA_FILE}" )
-  elseif( NOT EXISTS ${SCHEMA_FILE} ) # absolute path?
+    set( ${_res_var} "${SC_SOURCE_DIR}/data/${SCHEMA_FILE}" )
+  elseif( EXISTS ${SCHEMA_FILE} ) # already an absolute path
+    set( ${_res_var} ${SCHEMA_FILE} )
+  else()
     message( FATAL_ERROR "Cannot find ${CMAKE_BINARY_DIR}/${SCHEMA_FILE} or ${SC_SOURCE_DIR}/data/${SCHEMA_FILE}/*.exp or ${SCHEMA_FILE}" )
   endif()
 
-  if( IS_DIRECTORY ${SCHEMA_FILE} ) #if it is a dir, look for one .exp file inside
-    file(GLOB SCHEMA_FILE ${SCHEMA_FILE}/*.exp )
+  if( IS_DIRECTORY ${${_res_var}} ) #if it is a dir, look for one .exp file inside
+    file(GLOB ${_res_var} ${${_res_var}}/*.exp )
   endif()
 
-  if( NOT EXISTS ${SCHEMA_FILE} )
-    message(FATAL_ERROR "Expected one express file. Found '${SCHEMA_FILE}' instead.")
+  if( NOT EXISTS ${${_res_var}} )
+    message(FATAL_ERROR "Expected one express file. Found '${${_res_var}}' instead.")
   endif()
-ENDMACRO( LOCATE_SCHEMA SCHEMA_FILE )
+ENDMACRO( LOCATE_SCHEMA SCHEMA_FILE _res_var )
