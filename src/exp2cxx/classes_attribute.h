9e1d078b4622a4309a2e2fb41504b66a71a5f32a
Mark Pictor <mpictor@gmail.com>
2014-03-04 17:43:50 -0500

    add 2 protos to classes_attribute header

--- src/exp2cxx/classes_attribute.h
+++ src/exp2cxx/classes_attribute.h
@@ -4,6 +4,8 @@
 char * generate_attribute_name( Variable a, char * out );
 char * generate_attribute_func_name( Variable a, char * out );
 
+void ATTRprint_access_methods_get_head( const char * classnm, Variable a, FILE * file, bool returnsConst );
+void ATTRprint_access_methods_put_head( const char * entnm, Variable a, FILE * file );
 void ATTRsign_access_methods( Variable a, const char * objtype, FILE * file );
 void ATTRprint_access_methods( const char * entnm, Variable a, FILE * file );
 
