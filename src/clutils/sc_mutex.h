e67a5f51d1bb0b52a9c76d2e51b1486c9029a9a9
hoiji09 <bhangarval@gmail.com>
2014-06-15 13:32:17 +0530

    Added sc_mutex.h to src/clutils
    It is wrapper to std::mutex. It does nothing if HAVE_STD_MUTEX is not defined.

---
+++ src/clutils/sc_mutex.h
@@ -0,0 +1,31 @@
+#ifndef SC_MUTEX_H
+#define SC_MUTEX_H
+
+#ifdef HAVE_STD_THREAD
+# include <mutex>
+# include <thread>
+#endif //HAVE_STD_THREAD
+/*
+ This class is a wrapper to std::mutex.
+ It does nothing if HAVE_STD_MUTEX is not defined.
+*/
+class sc_mutex {
+    protected:
+#ifdef HAVE_STD_THREAD
+        std::mutex mtx;
+#endif //HAVE_STD_THREAD
+
+    public:
+        void lock() {
+#ifdef HAVE_STD_THREAD
+            mtx.lock();
+#endif //HAVE_STD_THREAD
+        }
+
+        void unlock() {
+#ifdef HAVE_STD_THREAD
+            mtx.unlock();
+#endif //HAVE_STD_THREAD
+        }
+};
+#endif //SC_MUTEX_H
