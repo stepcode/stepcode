10f041c779642474f36d234f561c0131352bef12
hoiji09 <bhangarval@gmail.com>
2014-07-18 14:46:45 +0530

    Made STEPattributeList thread safe
    Couple of functions were identified as vulnerable. The mutex defined in the superclass SingleLinkList was used.

--- src/clstepcore/STEPattributeList.cc
+++ src/clstepcore/STEPattributeList.cc
@@ -30,6 +30,7 @@ STEPattributeList::~STEPattributeList() {
 
 STEPattribute & STEPattributeList::operator []( int n ) {
     int x = 0;
+    mtxP->lock();
     AttrListNode * a = ( AttrListNode * )head;
     int cnt =  EntryCount();
     if( n < cnt ) {
@@ -39,12 +40,15 @@ STEPattribute & STEPattributeList::operator []( int n ) {
         }
     }
     if( a ) {
-        return *( a->attr );
+        STEPattribute * attr = a->attr;
+        mtxP->unlock();
+        return *( attr );
     }
 
     // else
     cerr << "\nERROR in STEP Core library:  " << __FILE__ <<  ":"
          << __LINE__ << "\n" << _POC_ << "\n\n";
+    mtxP->unlock();
     return *( STEPattribute * ) 0;
 }
 
@@ -53,11 +57,13 @@ int STEPattributeList::list_length() {
 }
 
 void STEPattributeList::push( STEPattribute * a ) {
+    mtxP->lock();
     AttrListNode * a2 = ( AttrListNode * )head;
 
     // if the attribute already exists in the list, don't push it
     while( a2 ) {
         if( *a == *( a2 -> attr ) ) {
+            mtxP->unlock();
             return;
         }
         a2 = ( AttrListNode * )( a2->next );
@@ -65,4 +71,5 @@ void STEPattributeList::push( STEPattribute * a ) {
     a->incrRefCount();
     AttrListNode * saln = new AttrListNode( a );
     AppendNode( saln );
+    mtxP->unlock();
 }
