57a3d9ca1acb836133da9792c337f7d91dc1ae6d
Mark Pictor <mpictor@gmail.com>
2015-02-12 20:40:37 -0500

    move BinaryAggregate and co out of STEPaggregate.*

--- src/clstepcore/STEPaggregate.h
+++ src/clstepcore/STEPaggregate.h
@@ -167,23 +167,7 @@ typedef  const StringAggregate *   StringAggregate_ptr_c;
 typedef        StringAggregate_ptr StringAggregate_var;
 
 
-/****************************************************************//**
-** \class BinaryAggregate
-** This class supports LIST OF BINARY type
-******************************************************************/
-class SC_CORE_EXPORT BinaryAggregate  :  public STEPaggregate {
-    public:
-        virtual SingleLinkNode * NewNode();
-        virtual STEPaggregate & ShallowCopy( const STEPaggregate & );
-
-        BinaryAggregate();
-        virtual ~BinaryAggregate();
-};
-typedef        BinaryAggregate *   BinaryAggregateH;
-typedef        BinaryAggregate *   BinaryAggregate_ptr;
-typedef  const BinaryAggregate *   BinaryAggregate_ptr_c;
-typedef        BinaryAggregate_ptr BinaryAggregate_var;
-
+#include "STEPaggrBinary.h"
 #include "STEPaggrEnum.h"
 #include "STEPaggrReal.h"
 #include "STEPaggrInt.h"
@@ -389,35 +373,7 @@ class SC_CORE_EXPORT StringNode  : public STEPnode {
         virtual SingleLinkNode   *  NewNode();
 };
 
-///////////////////////////////////////////////////////////////////////////
 
-/**************************************************************//**
-** \class BinaryNode
-** This class is for the Nodes of BinaryAggregates
-******************************************************************/
-class SC_CORE_EXPORT BinaryNode  : public STEPnode {
-    public:
-        SDAI_Binary  value;
-//  INPUT
-        virtual Severity StrToVal( const char * s, ErrorDescriptor * err );
-        virtual Severity StrToVal( istream & in, ErrorDescriptor * err );
-
-        virtual Severity STEPread( const char * s, ErrorDescriptor * err );
-        virtual Severity STEPread( istream & in, ErrorDescriptor * err );
-
-//  OUTPUT
-        virtual const char * asStr( std::string & s );
-        virtual const char * STEPwrite( std::string & s, const char * = 0 );
-        virtual void    STEPwrite( ostream & out = cout );
-
-//  CONSTRUCTORS
-        BinaryNode( BinaryNode & bn );
-        BinaryNode( const char * sStr );
-        BinaryNode();
-        ~BinaryNode();
-
-        virtual SingleLinkNode   *  NewNode();
-};
 
 
 
