ecde882d5a6575ad4ab9597797af4bfe51dbe22d
Mark Pictor <mpictor@gmail.com>
2015-07-26 14:32:43 -0400

    oops, forgot export macro for path2str

--- src/base/path2str.h
+++ src/base/path2str.h
@@ -1,13 +1,15 @@
 #ifndef PATH2STR_H
 #define PATH2STR_H
 
+#include <sc_export.h>
+
 /** windows only: rewrite backslashes in paths as forward slashes
  * call as path2str(__FILE__) to take advantage of macro
  *
  * silence "unknown escape sequence" warning when contents of __FILE__
  * are fprintf'd into string in generated code
  */
-const char * path2str_fn( const char * fileMacro );
+SC_BASE_EXPORT const char * path2str_fn( const char * fileMacro );
 
 #if defined( _WIN32 ) || defined ( __WIN32__ )
 #  define path2str(path) path2str_fn(path)
