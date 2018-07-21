f3b1065432420a2df54d637913bb38eadbc4004b
Chris HORLER <cshorler@googlemail.com>
2018-07-21 13:57:39 +0100

    additional tests / rename of tests for EXPresolve_op_dot

--- src/express/test/test_expr.c
+++ src/express/test/test_expr.c
@@ -13,10 +13,9 @@
 
 /* non-core */
 #include "express/dict.h"
+#include "express/variable.h"
 
 #include "fff.h"
-
-DEFINE_FFF_GLOBALS
 
 /*
  * missing header definition
@@ -55,21 +54,27 @@ Error ERROR_undefined_attribute;
  * mock functions
  */
 
+DEFINE_FFF_GLOBALS
+
 FAKE_VALUE_FUNC(int, EXPRESS_fail, Express)
 FAKE_VALUE_FUNC(Variable, ENTITYfind_inherited_attribute, struct Scope_ *, char *, struct Symbol_ **)
 FAKE_VALUE_FUNC(Variable, ENTITYresolve_attr_ref, Entity, Symbol *, Symbol *)
 FAKE_VALUE_FUNC(struct Scope_ *, ENTITYfind_inherited_entity, struct Scope_ *, char *, int)
-FAKE_VALUE_FUNC(Variable, VARcreate, Expression, Type)
 FAKE_VOID_FUNC(EXP_resolve, Expression, Scope, Type)
 
 void setup() {
-    Type_Identifier = TYPEcreate(identifier_);    
+    Type_Identifier = TYPEcreate(identifier_);
+    Type_Attribute = TYPEcreate(attribute_);
+    
+    RESET_FAKE(EXPRESS_fail);
+    RESET_FAKE(ENTITYfind_inherited_attribute);
+    RESET_FAKE(ENTITYresolve_attr_ref);
+    RESET_FAKE(ENTITYfind_inherited_entity);
+    RESET_FAKE(EXP_resolve);
 }
 
-/*
- * mock resolve operation
- */ 
-void EXP_resolve_handler(Expression exp, Scope cxt, Type typ) {
+/* TODO: remove DICTlookup after eliminating DICT_type */
+void EXP_resolve_type_handler(Expression exp, Scope cxt, Type typ) {
     (void) typ;
     Type res_typ = DICTlookup(cxt->symbol_table, exp->symbol.name); 
     exp->type = res_typ;
@@ -77,7 +82,7 @@ void EXP_resolve_handler(Expression exp, Scope cxt, Type typ) {
     exp->symbol.resolved = RESOLVED;
 }
 
-int test_resolve_op_dot() {
+int test_resolve_select_enum_member() {
     Schema scope;
     Symbol *e_type_id, *enum_id, *s_type_id;
     Type enum_typ, select_typ, chk_typ;
@@ -126,13 +131,83 @@ int test_resolve_op_dot() {
      * test: sel_ref '.' enum_id
      * expectation: enum_typ
      */
-    EXP_resolve_fake.custom_fake = EXP_resolve_handler;
+    EXP_resolve_fake.custom_fake = EXP_resolve_type_handler;
     
     chk_typ = EXPresolve_op_dot(expr, scope);
 
     assert(EXP_resolve_fake.call_count == 1);
     assert(expr->e.op1->type == select_typ);
     assert(chk_typ == enum_typ);
+    
+    /* in case of error SIGABRT will be raised (and non-zero returned) */
+    
+    return 0;
+}
+
+/* TODO: remove DICTlookup after eliminating DICT_type */
+void EXP_resolve_entity_handler(Expression exp, Scope cxt, Type unused) {
+    (void) unused;
+    Entity ent = DICTlookup(cxt->symbol_table, exp->symbol.name);
+    Type typ = ent->u.entity->type;
+    exp->type = typ;
+    exp->return_type = typ;
+    exp->symbol.resolved = RESOLVED;
+}
+
+Variable ENTITY_resolve_attr_handler(Entity ent, Symbol *grp_ref, Symbol *attr_ref) {
+    (void) grp_ref;
+    Variable v = DICTlookup(ent->symbol_table, attr_ref->name);
+    return v;   
+}
+
+int test_resolve_entity_attribute() {
+    Schema scope;
+    Symbol *e_type_id, *attr_id;
+    Entity ent;
+    Type attr_typ, chk_typ;
+    TypeBody tb;
+    Expression exp_attr, expr, op1, op2;
+    Variable var_attr;
+    Linked_List explicit_attr_list;
+
+    /*
+     * boilerplate: create objects, populate symbol tables
+     */
+    scope = SCHEMAcreate();
+
+    e_type_id = SYMBOLcreate("entity1", 1, "test2");
+    ent = ENTITYcreate(e_type_id);
+    DICT_define(scope->symbol_table, e_type_id->name, ent, &ent->symbol, OBJ_ENTITY);
+
+    attr_id = SYMBOLcreate("attr1", 1, "test2");
+    exp_attr = EXPcreate_from_symbol(Type_Attribute, attr_id);    
+    tb = TYPEBODYcreate(number_);
+    attr_typ = TYPEcreate_from_body_anonymously(tb);
+    attr_typ->superscope = ent;
+    var_attr = VARcreate(exp_attr, attr_typ);
+    var_attr->flags.attribute = 1;
+    explicit_attr_list = LISTcreate();
+    LISTadd_last(explicit_attr_list, var_attr);
+    
+    LISTadd_last(ent->u.entity->attributes, explicit_attr_list);
+    DICTdefine(ent->symbol_table, attr_id->name, var_attr, &var_attr->name->symbol, OBJ_VARIABLE);
+
+    op1 = EXPcreate_from_symbol(Type_Identifier, e_type_id);
+    op2 = EXPcreate_from_symbol(Type_Attribute, attr_id);
+    expr = BIN_EXPcreate(OP_DOT, op1, op2);
+    
+    /*
+     * test: entity_ref '.' attribute_id
+     * expectation: attr_typ
+     */
+    EXP_resolve_fake.custom_fake = EXP_resolve_entity_handler;
+    ENTITYresolve_attr_ref_fake.custom_fake = ENTITY_resolve_attr_handler;
+    
+    chk_typ = EXPresolve_op_dot(expr, scope);
+
+    assert(EXP_resolve_fake.call_count == 1);
+    assert(expr->e.op1->type == ent->u.entity->type);
+    assert(chk_typ == attr_typ);
     
     /* in case of error SIGABRT will be raised (and non-zero returned) */
     
@@ -148,7 +223,8 @@ int main(int argc, char *argv[]) {
     int status;
  
     struct test_def tests[] = {
-        {"resolve_op_dot", test_resolve_op_dot}
+        {"resolve_select_enum_member", test_resolve_select_enum_member},
+        {"resolve_entity_attribute", test_resolve_entity_attribute}
     };
     
     /* enable libexpress allocator */
@@ -179,6 +255,7 @@ int main(int argc, char *argv[]) {
         /* all tests */
         for (unsigned int j=0; j < (sizeof tests / sizeof tests[0]); j++) {
             int (*test_ptr) (void) = tests[j].testfunc;
+            setup();
             status |= test_ptr();
         }
     }
@@ -186,4 +263,4 @@ int main(int argc, char *argv[]) {
     return status;
 }
 
-/* TODO: additional test for entity_ attribute, variable */
+
