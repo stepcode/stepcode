b9d1b135c2e653ab9c0360a3fec965ade597be4c
Chris HORLER <cshorler@googlemail.com>
2018-07-22 18:40:41 +0100

    initial test harness for express.c, resolve.c, schema.c, scope.c, type.c (currently no tests)

--- src/express/test/test_resolve.c
+++ src/express/test/test_resolve.c
@@ -1,7 +1,70 @@
+#include <assert.h>
+
 #include "express/resolve.h"
 
-int main(int argc, char *argv[]) {
+/* core */
+#include "express/hash.h"
+#include "express/linklist.h"
 
+/* non-core */
+#include "express/type.h"
+
+#include "driver.h"
+#include "fff.h"
+
+/*
+ * mock globals
+ */
+
+char * EXPRESSprogram_name;
+int yylineno;
+int __SCOPE_search_id;
+int EXPRESSpass;
+struct Scope_ * FUNC_NVL;
+struct Scope_ * FUNC_USEDIN;
+
+Type Type_Unknown;
+Type Type_Dont_Care;
+Type Type_Bad;
+Type Type_Runtime;
+Type Type_Attribute;
+Type Type_Self;
+Type Type_Bag_Of_Generic;
+Type Type_Funcall;
+
+struct EXPop_entry EXPop_table[OP_LAST];
+
+int tag_count;
+
+/*
+ * mock functions
+ */
+
+DEFINE_FFF_GLOBALS
+
+FAKE_VALUE_FUNC(Generic, SCOPEfind, Scope, char *, int)
+FAKE_VALUE_FUNC(Variable, VARfind, Scope, char *, int)
+FAKE_VALUE_FUNC(char *, VARget_simple_name, Variable)
+FAKE_VALUE_FUNC(struct Scope_ *, ENTITYfind_inherited_entity, struct Scope_ *, char *, int)
+FAKE_VALUE_FUNC(Variable, ENTITYresolve_attr_ref, Entity, Symbol *, Symbol *)
+FAKE_VALUE_FUNC(Variable, ENTITYget_named_attribute, Entity, char *)
+FAKE_VALUE_FUNC(int, ENTITYdeclares_variable, Entity, Variable)
+FAKE_VALUE_FUNC(int, EXPRESS_fail, Express)
+
+void setup() {
+    RESET_FAKE(SCOPEfind);
+    RESET_FAKE(VARfind);
+    RESET_FAKE(VARget_simple_name);
+    RESET_FAKE(ENTITYfind_inherited_entity);
+    RESET_FAKE(ENTITYresolve_attr_ref);
+    RESET_FAKE(ENTITYget_named_attribute);
+    RESET_FAKE(ENTITYdeclares_variable);
+    RESET_FAKE(EXPRESS_fail);    
+}
+
+int test_something() {
+
+/*
     EXP_resolve();
     
     ENTITYresolve_subtype_expression();
@@ -16,8 +79,14 @@ int main(int argc, char *argv[]) {
     
     ENTITYresolve_supertypes();
     
-    SCOPEresolve_expressions_statements();
-    
-    return 0;
+    SCOPEresolve_expressions_statements();*/
+  
+    return 1;
 }
+
+struct test_def tests[] = {
+    {"resolve_select_enum_member", test_something},
+    {"resolve_entity_attribute", test_something},
+    {NULL}
+};
 
--- src/express/test/test_schema.c
+++ src/express/test/test_schema.c
@@ -1,14 +1,55 @@
+#include <assert.h>
+
 #include "express/schema.h"
 
-int main(int argc, char *argv[]) {
+/* core */
+#include "express/hash.h"
+#include "express/linklist.h"
 
-    SCHEMAdefine_reference();
+/* non-core */
+
+#include "driver.h"
+#include "fff.h"
+
+/*
+ * mock globals
+ */
+char * EXPRESSprogram_name;
+int yylineno;
+
+int ENTITY_MARK;
+
+
+/*
+ * mock functions
+ */
+
+DEFINE_FFF_GLOBALS
+
+FAKE_VALUE_FUNC(int, EXPRESS_fail, Express)
+FAKE_VOID_FUNC(SCOPE_get_entities, Scope, Linked_List)
+FAKE_VALUE_FUNC(Variable, ENTITYfind_inherited_attribute, struct Scope_ *, char *, struct Symbol_ **)
+
+void setup() {
+    RESET_FAKE(EXPRESS_fail)
+    RESET_FAKE(SCOPE_get_entities)
+    RESET_FAKE(ENTITYfind_inherited_attribute)
+}
+
+int test_something() {
+    /* SCHEMAdefine_reference();
 
     SCHEMAdefine_use();
 
     SCHEMA_get_entities_ref();
 
-    VARfind();
+    VARfind();*/
     
-    return 0;
+    return 1;
 }
+
+struct test_def tests[] = {
+    {"resolve_select_enum_member", test_something},
+    {"resolve_entity_attribute", test_something},
+    {NULL}
+};
