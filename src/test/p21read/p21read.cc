fe796629a2938b5fec76377aba52074e3d978785
Cliff Yapp <238416+starseeker@users.noreply.github.com>
2020-12-15 14:49:54 -0500

    Fix p21read compilation.

--- src/test/p21read/p21read.cc
+++ src/test/p21read/p21read.cc
@@ -155,7 +155,7 @@ int main(int argc, char *argv[])
     char     *flnm;
 
     // p21 ReadExchangeFile()
-    benchmark stats();
+    benchmark stats;
 
     cout << argv[0] << ": load file ..." << endl;
     if(argc >= (sc_optind + 1)) {
